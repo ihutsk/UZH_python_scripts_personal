65d50f416d38a5102422edb1c48c4aba92d80076
ihutsk <159417966+ihutsk@users.noreply.github.com>
2024-10-20 18:28:14 +0200

    Script to make input for crest to remove the duplicates

---
+++ Thesis/xtb_orca/isopropyl/orca_output_to_crest.py
@@ -0,0 +1,35 @@
+
+import os, re
+import pandas as pd
+
+# Enter number of atoms in your structure to `num_atoms`.
+
+num_atoms = 252
+
+
+df = pd.read_excel('comparison.xlsx')
+
+
+# list of folders
+folders = []
+for dr in os.scandir():
+    if dr.is_dir() and 'structure' in dr.name and 'imaginary' not in dr.name:
+        folders.append(dr.name)
+
+
+pattern_xyz = 'struc_[0-9]+_r2scan_opt\.xyz'
+
+
+with open('ensemble_after_orca_with_duplicates.xyz', 'a', encoding='utf-8') as f:
+    for folder in folders:
+        xyz_name = [file for file in os.listdir(folder) if re.search(pattern=pattern_xyz, string=file)][0]
+        with open(f'{folder}/{xyz_name}', 'r', encoding='utf-8') as xyz:
+            lines = xyz.readlines()
+            coords = lines[2:]
+            energy = str(df[df['name'] == folder].iloc[0,5])
+            f.write(f'{num_atoms}\n{energy}\n')
+            f.write(''.join(coords))
+    
+
+
+
